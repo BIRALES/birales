ca6067459d17600b47eeb001b2c2a8c87fe1bb54
Denis <denis0@gmail.com>
2016-03-14 09:58:37 +0100

    Refactoring of code for next implementation stage

--- src/app/sandbox/postprocessing/BeamData.py
+++ src/app/sandbox/postprocessing/BeamData.py
@@ -1,7 +1,7 @@
 from plotly.offline import plot
-from plotly.tools import FigureFactory
 import plotly.graph_objs as go
 import numpy as np
+from helpers import LineGeneratorHelper
 
 
 class BeamData:
@@ -11,11 +11,18 @@ class BeamData:
     def __init__(self):
         self.frequencies = np.linspace(0, 200, 200)
         self.time = 600
-        self.power = []
+        self.power = self.mock_up()  # use mock up data for now
 
-        self.mock_up()  # use mock up data for now
+    def read_data(self, file_name):
+        return
 
     def visualise(self, name):
+        """
+        Visualise the Beam Data as a Heatmap
+
+        :param name: Name of the file
+        :return: Void
+        """
         data = [
             go.Heatmap(
                 z = self.power,
@@ -47,88 +54,23 @@ class BeamData:
 
         return noiseless_data + noise
 
-    def mock_up2(self):
-        count = 0
-
-        power = []
-        for f in self.frequencies:
-            count += 1
-            intensity = np.zeros(self.time)
-            # intensity = self.add_noise(intensity)
-
-            if 150 < f < 180:
-                if count % 5 == 0:
-                    intensity[count] = 1.0
-
-            power.append(intensity)
-
-        self.power = power
-
-        self.visualise('Mockup of Beam Data')
-
-    def mock_up(self):
-        count = 0
-
+    def mock_up(self, visualise = False):
+        """
+        Build sample / mock up data to be used for testing
+        :param visualise: Plot the mock data
+        :return: power
+        """
         power = np.zeros((max(self.frequencies), self.time))
-        line_coor = BeamData.get_line((90, 120), (400, 180))  # (x0, y0), (x1, y1)
+        line_coor = LineGeneratorHelper.get_line((90, 120), (400, 180))  # (x0, y0), (x1, y1)
 
         for coordinate in line_coor:
             x = coordinate[0]
             y = coordinate[1]
-            # print x, y
             power[y][x] = 1.0
-        # self.visualise('Mockup of Beam Data')
 
-        self.power = power
-        # self.visualise('Mockup of Beam Data')
+        if visualise:
+            self.visualise('Mockup of Beam Data')
 
-        print 'Detection has', len(line_coor), 'pixels'
+        print 'Detection has', len(line_coor),
 
-    @staticmethod
-    def get_line(start, end):
-        """Bresenham's Line Algorithm
-        """
-        # Setup initial conditions
-        x1, y1 = start
-        x2, y2 = end
-        dx = x2 - x1
-        dy = y2 - y1
-
-        # Determine how steep the line is
-        is_steep = abs(dy) > abs(dx)
-
-        # Rotate line
-        if is_steep:
-            x1, y1 = y1, x1
-            x2, y2 = y2, x2
-
-        # Swap start and end points if necessary and store swap state
-        swapped = False
-        if x1 > x2:
-            x1, x2 = x2, x1
-            y1, y2 = y2, y1
-            swapped = True
-
-        # Recalculate differentials
-        dx = x2 - x1
-        dy = y2 - y1
-
-        # Calculate error
-        error = int(dx / 2.0)
-        ystep = 1 if y1 < y2 else -1
-
-        # Iterate over bounding box generating points between start and end
-        y = y1
-        points = []
-        for x in range(x1, x2 + 1):
-            coord = (y, x) if is_steep else (x, y)
-            points.append(coord)
-            error -= abs(dy)
-            if error < 0:
-                y += ystep
-                error += dx
-
-        # Reverse the list if the coordinates were swapped
-        if swapped:
-            points.reverse()
-        return points
+        return power
--- src/app/sandbox/postprocessing/OrbitDeterminationInputGenerator.py
+++ src/app/sandbox/postprocessing/OrbitDeterminationInputGenerator.py
@@ -4,19 +4,14 @@ from plotly.offline import plot
 import plotly.graph_objs as go
 from skimage.transform import hough_line
 from skimage.transform import hough_line_peaks
-from scipy import signal
 from helpers import TableMakerHelper
-from helpers import DateTimeHelper
+from helpers import LineGeneratorHelper
 from BeamData import BeamData
-
-from skimage.morphology import disk
-from skimage.filters.rank import median
-from scipy import ndimage
-import matplotlib.pyplot as plt
 
 
 class OrbitDeterminationInputGenerator:
-    mock_data_dir = 'data/'
+    mock_data_dir = 'results/'
+    data_dir = 'results/'
     output_dir = 'output/'
     max_detections = 3
     max_frequency = 200
@@ -26,69 +21,33 @@ class OrbitDeterminationInputGenerator:
         self.min_time = 0
         return
 
-    @staticmethod
-    def get_line(start, end):
-        """Bresenham's Line Algorithm
-        """
-
-        # Setup initial conditions
-        x1, y1 = start
-        x2, y2 = end
-        dx = x2 - x1
-        dy = y2 - y1
-
-        # Determine how steep the line is
-        is_steep = abs(dy) > abs(dx)
-
-        # Rotate line
-        if is_steep:
-            x1, y1 = y1, x1
-            x2, y2 = y2, x2
-
-        # Swap start and end points if necessary and store swap state
-        swapped = False
-        if x1 > x2:
-            x1, x2 = x2, x1
-            y1, y2 = y2, y1
-            swapped = True
-
-        # Recalculate differentials
-        dx = x2 - x1
-        dy = y2 - y1
-
-        # Calculate error
-        error = int(dx / 2.0)
-        ystep = 1 if y1 < y2 else -1
-
-        # Iterate over bounding box generating points between start and end
-        y = y1
-        points = []
-        for x in range(x1, x2 + 1):
-            coord = (y, x) if is_steep else (x, y)
-            points.append(coord)
-            error -= abs(dy)
-            if error < 0:
-                y += ystep
-                error += dx
-
-        # Reverse the list if the coordinates were swapped
-        if swapped:
-            points.reverse()
-        return points
-
-    def mock_data(self, file_name):
+    def mock_result(self, file_name):
         with open(self.mock_data_dir + file_name) as csv_file:
             row = csv.reader(csv_file)
             mock_data = list(row)
 
         return mock_data
 
+    def visualise_mock_result(self):
+        table = TableMakerHelper()
+        table.set_headers([
+            'Epoch',
+            'MJD2000',
+            'Time Delay',
+            'Doppler',
+            'SNR'
+        ])
+
+        mock_data = self.mock_data('beam_sample_output.csv')
+        table.set_rows(mock_data)
+
+        table.visualise_with_graph('Beam_4')
+
     def visualise_hough_transform(self, name, hough_transform_data):
         data = [
             go.Heatmap(
                 z = hough_transform_data,
                 colorscale = 'Viridis',
-                # colorbar = {'title': 'Hough Transform'},
             )
         ]
 
@@ -103,20 +62,7 @@ class OrbitDeterminationInputGenerator:
 
         plot(fig, filename = self.output_dir + name + '.html')
 
-    def visualise_mock_data(self):
-        table = TableMakerHelper()
-        table.set_headers([
-            'Epoch',
-            'MJD2000',
-            'Time Delay',
-            'Doppler',
-            'SNR'
-        ])
 
-        mock_data = self.mock_data('beam_sample_output.csv')
-        table.set_rows(mock_data)
-
-        table.visualise_with_graph('Beam_4')
 
     def visualise_line(self, name, image, lines_xs = None, lines_ys = None):
         traces = []
@@ -165,26 +111,12 @@ class OrbitDeterminationInputGenerator:
             y0 = (dist - x0 * np.cos(angle)) / np.sin(angle)
             y1 = (dist - x1 * np.cos(angle)) / np.sin(angle)
 
-            c = y0
-            m = self.gradient(y0, y1, x0, x1)
-
-            # print y0, y1
-            # y = []
-            # for x in range(0, self.max_frequency):
-            #     yy = m * x + c
-            #
-            #     y.append(m * x + c)
             ys.append((y0, y1))
             xs.append((x0, x1))
-            print angle
-            print 'y=', m, 'x +', y0
 
         print 'There were', len(angles), 'detections'
 
         return xs, ys
-
-    def gradient(self, y0, y1, x0, x1):
-        return (y1 - y0) / (x1 - x0)
 
     def remove_noise(self, noisy_data):
         # Remove instaces that are 3 stds away from the mean
@@ -228,27 +160,6 @@ class OrbitDeterminationInputGenerator:
                 else:
                     image[y][x] = 0.0  # paint line
         print 'There were', hits, 'hits'
-        return image
-
-    def print_line_as_heatmap(self, image, xs, ys):
-        z = []
-        xs = xs[0]
-
-        # print ys[0][0:10]
-        print np.nonzero(image[170])  # y=170, x= 171
-        print 'ys', np.round(ys[0])[170:180].tolist()
-        print 'xs', xs[170:180]
-        ys = np.round(ys[0])
-
-        print len(image[0]), len(image), len(ys), len(xs)
-
-        # print xs[0:10]
-
-        for x in range(0, self.max_frequency):
-            y = int(ys[x])
-            if y >= 0:
-                image[y][x] = 2.0
-                # print x, y
         return image
 
 
