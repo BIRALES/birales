3c2a1cbc6f369617b933c7244b6146c9316e5732
Denis <denis0@gmail.com>
2016-10-31 17:29:44 +0100

    deleted candidate data and replaced it with detections
    Former-commit-id: 9f2dedab687e3cc3078bd2d3d81210d29111d372

--- python/postprocessing/core/detection_candidates.py
+++ python/postprocessing/core/detection_candidates.py
@@ -27,16 +27,6 @@ class BeamSpaceDebrisCandidate:
         self.name = name
         self.id = self.beam.observation_name + '.' + str(self.beam.id) + '.' + str(name)
         self.detection_data = detection_data
-
-        self.data = {
-            'time': [],
-            'mdj2000': [],
-            'time_elapsed': [],
-            'frequency': [],
-            'doppler_shift': [],
-            'snr': [],
-        }
-
         self.detections = []
 
         # todo - This function can be called lazily, since it is only for visualisation
@@ -44,13 +34,6 @@ class BeamSpaceDebrisCandidate:
 
     def set_data(self, detection_data):
         for frequency, time, snr in sorted(detection_data, key=lambda row: row[1]):
-            self.data['time'].append(time)
-            self.data['mdj2000'].append(self._get_mdj2000(time))
-            self.data['time_elapsed'].append(self._time_elapsed(time))
-            self.data['frequency'].append(frequency)
-            self.data['doppler_shift'].append(self._get_doppler_shift(self.beam.tx, frequency))
-            self.data['snr'].append(snr)
-
             self.detections.append({
                 'time': time,
                 'mdj2000': self._get_mdj2000(time),
@@ -75,7 +58,6 @@ class BeamSpaceDebrisCandidate:
     def __iter__(self):
         yield '_id', self.id
         yield 'name', self.name
-        yield 'data', self.data
         yield 'detections', self.detections
         yield 'beam_id', self.beam.id
         yield 'data_set_id', self.beam.data_set.id
