4d8b948ca10c6a01c71a33fb8a4d5a5b591b211f
denis <denis0@gmail.com>
2017-04-12 14:25:13 +0200

    added orbit determination table

--- python/pybirales/modules/monitoring/server.py
+++ python/pybirales/modules/monitoring/server.py
@@ -4,13 +4,18 @@ from flask import Flask, render_template, Response, request
 from pybirales.modules.detection.repository import BeamCandidateRepository
 from datetime import datetime
 from bson import json_util
-from astropy.time import Time
+from astropy.time import Time, TimeDelta
 
 # Initialize the Flask application
 app = Flask(__name__)
 
 beam_candidates_repo = BeamCandidateRepository()
 
+MIN_CHANNEL = 409.9921875
+MAX_CHANNEL = 410.0703125
+
+MIN_CHANNEL = 4
+MAX_CHANNEL = 410.0703125
 
 @app.route('/monitoring')
 def index():
@@ -39,12 +44,14 @@ def get_beam_candidates():
 
 @app.route('/monitoring/beam_candidates/table', methods=['GET', 'POST'])
 def get_orbit_determination_table():
-    beam_id = request.args.get('beam_id', type=int)
-    max_channel = request.args.get('max_channel', type=float)
-    min_channel = request.args.get('min_channel', type=float)
+    now = Time.now()
+    beam_id = request.args.get('beam_id', type=int) if request.args.get('beam_id') else None
+    max_channel = request.args.get('max_channel', type=float) if request.args.get('max_channel') else None
+    min_channel = request.args.get('min_channel', type=float) if request.args.get('min_channel') else None
 
-    max_time = datetime.strptime(request.args.get('max_time'), '%a, %d %b %Y %H:%M:%S %Z')
-    min_time = datetime.strptime(request.args.get('min_time'), '%a, %d %b %Y %H:%M:%S %Z')
+    max_time = Time(request.args.get('max_time')).datetime if request.args.get('max_time') else now.datetime
+    min_time = Time(request.args.get('min_time')).datetime if request.args.get('min_time') else (
+    now - TimeDelta(3600, format='sec')).datetime
 
     detected_beam_candidates = beam_candidates_repo.get(beam_id, max_channel, min_channel, max_time, min_time)
 
