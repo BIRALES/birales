db6b69c35cea86d9baeaac97d815f472ff0090ae
Denis Cutajar <denis0@gmail.com>
2017-11-26 13:58:02 +0100

    put update config in cli

--- pybirales/cli/cli.py
+++ pybirales/cli/cli.py
@@ -4,6 +4,16 @@ from commands.pipelines import pipelines
 from commands.services import services
 
 
+def update_config(config, section, key, value):
+    if config:
+        if section in config:
+            config[section][key] = value
+            return config
+        config[section] = {}
+        return update_config(config, section, key, value)
+    return update_config({}, section, key, value)
+
+
 @click.group()
 @click.pass_context
 def cli(ctx):
--- pybirales/cli/commands/pipelines.py
+++ pybirales/cli/commands/pipelines.py
@@ -1,16 +1,7 @@
 import click
 from pybirales.birales import BiralesFacade, BiralesConfig
 from pybirales.pipeline.pipeline import DetectionPipelineMangerBuilder, CorrelatorPipelineManagerBuilder
-
-
-def update_config(config, section, key, value):
-    if config:
-        if section in config:
-            config[section][key] = value
-            return config
-        config[section] = {}
-        return update_config(config, section, key, value)
-    return update_config({}, section, key, value)
+from pybirales.cli.cli import update_config
 
 
 @click.group()
