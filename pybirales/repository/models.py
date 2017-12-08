697745319bd7389fcf6bb452c3f2193a6bfc0baa
Denis Cutajar <denis0@gmail.com>
2017-12-08 15:00:03 +0100

    added space debris candidate model

--- pybirales/repository/models.py
+++ pybirales/repository/models.py
@@ -3,13 +3,31 @@ import datetime
 import warnings
 from mongoengine import *
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
+# with warnings.catch_warnings():
+#     warnings.simplefilter("ignore")
 
 
 class Observation(Document):
-    meta = {'allow_inheritance': True}
+    id = ObjectIdField()
     name = StringField(required=True, max_length=200)
     date_time_start = DateTimeField(required=True, default=datetime.datetime.utcnow)
     date_time_end = DateTimeField()
     settings = DynamicField()
+
+
+class SpaceDebrisCandidate(Document):
+    id = ObjectIdField()
+    observation = ReferenceField(Observation, required=True)
+    beam_id = IntField(required=True)
+    beam_ra = FloatField(required=True)
+    beam_dec = FloatField(required=True)
+    created_at = DateTimeField(default=datetime.datetime.utcnow)
+
+    min_time = DateTimeField(required=True)
+    max_time = DateTimeField(required=True)
+    max_channel = FloatField(required=True)
+    min_channel = FloatField(required=True)
+
+    snr_data = ListField()
+    channel_data = ListField()
+    time_data = ListField()
