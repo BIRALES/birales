a8cb7a72820eaddd1f19306b5063770db7349436
Denis Cutajar <denis0@gmail.com>
2017-12-14 10:39:54 +0100

    added construct

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
                       "flask_ini",
                       "fadvise",
                       'sklearn', 'pandas', 'webargs', 'yappi', 'marshmallow', 'humanize', 'mongoengine', 'pyfits',
+                      'construct==2.5.5-reupload',
                       'corr'],
     data_files=[(os.environ['HOME'] + '/.birales', ['pybirales/configuration/local.ini']),
                 (os.environ['HOME'] + '/.birales/tcpo', []),
