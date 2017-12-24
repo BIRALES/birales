3a9fdc052035d2443ef96d67aa921657c7981107
Denis Cutajar <denis0@gmail.com>
2017-12-24 20:31:01 +0100

    Added new scheduler CLI entry point to reflect latest changes

---
+++ pybirales/cli/commands/scheduler_2.py
@@ -0,0 +1,29 @@
+import logging as log
+import sys
+
+import click
+from pybirales.services.scheduler.scheduler import ObservationsScheduler
+
+
+@click.command()
+@click.option('--schedule_file', '-s', 'schedule_file_path', type=click.Path(exists=True), required=True,
+              help='The scheduler json file')
+@click.option('--format', '-f', 'file_format', default='json', help='The format of the schedule file [tdm/json]')
+def scheduler(schedule_file_path, file_format):
+    """
+    Schedule a series of observations
+
+    :param schedule_file_path: The file path to the schedule
+    :param file_format: The format of the schedule file [tdm/json]
+    :return:
+    """
+
+    try:
+        s = ObservationsScheduler()
+        s.load_from_file(schedule_file_path, file_format)
+        s.start()
+    except KeyboardInterrupt:
+        log.info('Ctrl-C received. Terminating the scheduler process.')
+        sys.exit()
+    else:
+        log.info('Scheduler finished successfully.')
