6677a06bfbafe8eb69a799b523b99768c47c3a13
Denis Cutajar <denis0@gmail.com>
2018-01-15 11:30:47 +0100

    added deploy script (skeleton for now)

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+# Check that the dependencies are met. If not install.
+
+# Deploy the BIRALES application
+python setup.py install
+
+# Compile any C code
+
+# Change user permission for the BIRALES home folder
+chown -R lessju:lessji ~/.birales
