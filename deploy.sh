cab78f8ee786c573b0119d8d19d170efeabd6a98
Denis Cutajar <denis0@gmail.com>
2018-01-18 17:16:17 +0100

    added more directives to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,16 @@
 # Deploy the BIRALES application
 python setup.py install
 
-# Compile any C code
+# Compile the beamformer library
+cd pybirales/pipeline/beamformer/src
+mkdir build
+cd build
+cmake ..
+sudo  make install
+
+# Create the logging directory
+mkdir /var/log/birales
+chown -R lessju:lessji /var/log/birales
 
 # Change user permission for the BIRALES home folder
 chown -R lessju:lessji ~/.birales
