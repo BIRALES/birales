re-factoring of the detection module deleted dead code fixed doppler range window fixed doppler gradient range (it is physical now) added noise per beam remove detection clusters and replaced them with numpy arrays

pybirales/pipeline/modules/detection/dbscan_detection.py
@@ -1,180 +1,206 @@
 import logging as log
-import os
-import warnings
-
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 import numpy as np
-from astropy.io import fits
+import pandas as pd
+from scipy import stats
 from sklearn import linear_model
 from sklearn.cluster import DBSCAN
 
 from pybirales import settings
-from pybirales.events.events import SpaceDebrisClusterDetectedEvent
-from pybirales.events.publisher import EventsPublisher
+from pybirales.pipeline.modules.detection.exceptions import NoDetectionClustersFound
 from pybirales.pipeline.base.timing import timeit
-from pybirales.pipeline.modules.detection.detection_clusters import DetectionCluster
-
-
 
 _eps = 5
 _min_samples = 5
 _algorithm = 'kd_tree'
 _linear_model = linear_model.RANSACRegressor(linear_model.LinearRegression())
 db_scan = DBSCAN(eps=_eps, min_samples=_min_samples, algorithm=_algorithm, n_jobs=-1)
-_ref_time = None
-_time_delta = None
-_publisher = None
+N_SAMPLES = 32
 
 
-def _db_scan(beam_id, data):
+def _validate(channel, time_sample, td):
     """
-    Perform the db_scan algorithm on the beam data to identify clusters
+    Validate the detection cluster
 
+    :param channel_ndx:
+    :param time_ndx:
+    :return:
+
+    """
+
+    # Check if cluster is not small
+    if channel.shape[0] < 3:
+        # log.debug('Rejecting cluster with n: %s', channel_ndx.shape[0])
+        return False
+
+    m, _, r_value, _, _ = stats.linregress(time_sample, channel)
+
+    # Correct the gradient's units to be in Hz/s
+    m = m * 1e6 / (td / np.timedelta64(1, 's'))
+
+    # Check if cluster is linear
+    if np.abs(r_value) < settings.detection.linearity_thold:
+        # log.debug('Rejecting cluster with r: %s', r_value)
+        return False
+
+    # Check if cluster gradient is within doppler gradient window
+    if settings.detection.enable_gradient_thold:
+        if not (settings.detection.gradient_thold[0] >= m >= settings.detection.gradient_thold[1]):
+            # log.debug('Rejecting cluster with m: %s', m)
+            return False
+
+    return True
+
+
+def _create(snr_data, cluster_data, channels, t0, td, beam_id, iter_count):
+    """
+
+    :param cluster_data: The labelled cluster data
+    :param channels:
+    :param t0:
+    :param td:
     :param beam_id:
-    :param data:
+    :param iter_count:
     :return:
     """
 
-    try:
-        # Perform clustering on the data and returns cluster labels the points are associated with
-        return db_scan.fit_predict(data)
-    except ValueError:
-        return []
+    channel_ndx = cluster_data[:, 0]
+    time_ndx = cluster_data[:, 1]
+
+    time_sample = time_ndx + iter_count * N_SAMPLES
+
+    # Calculate the channel (frequency) of the sample from the channel index
+    channel = channels[channel_ndx]
+
+    # Check if the cluster is a valid cluster
+    if not _validate(channel, time_sample, td):
+        return False, None
+
+    # Calculate the time of the sample form the time index
+    time = t0 + time_ndx + iter_count * td * N_SAMPLES
+
+    return True, pd.DataFrame({
+        'time_sample': time_sample,
+        'channel_sample': channel_ndx,
+        'time': time,
+        'channel': channel,
+        'snr': snr_data,
+        'beam_id': np.full(time_ndx.shape[0], beam_id),
+        'iter': np.full(time_ndx.shape[0], iter_count),
+    })
 
 
-def check_doppler_range(channels, tx):
-    # If Doppler filtering is disabled, all candidates are valid
-    if not settings.detection.doppler_subset:
-        return True
-
-    dp = (channels - tx) * 1e6
-    return (dp > settings.detection.doppler_range[0]).all() and (dp < settings.detection.doppler_range[1]).all()
-
-
-def _create_detection_clusters(data, beam, cluster_labels, label):
-    data_indices = data[np.where(cluster_labels == label)]
-
-    log.debug('Beam %s: cluster %s contains %s data points', beam.id, label, len(data_indices[:, 1]))
-
-    channel_indices = data_indices[:, 0]
-    time_indices = data_indices[:, 1]
-
-    # If cluster is 'small' do not consider it
-    if len(channel_indices) < _min_samples:
-        log.debug('Beam %s: Ignoring small cluster with %s data points', beam.id, len(channel_indices))
-        return None
-
-    # If cluster is not within the physical doppler range limits
-    if not check_doppler_range(beam.channels[channel_indices], beam.tx):
-        return None
-
-    # Create a Detection Cluster from the cluster data
-    cluster = DetectionCluster(model=_linear_model,
-                               beam_config=beam.get_config(),
-                               time_data=beam.time[time_indices],
-                               channels=beam.channels[channel_indices],
-                               channels_i=beam.channels_i[channel_indices],
-                               snr=beam.snr[(channel_indices, time_indices)])
-
-    # Add only those clusters that are linear
-    if cluster.is_linear and cluster.is_valid:
-        log.debug('Beam %s: Cluster with m:%3.2f, c:%3.2f, n:%s and r:%0.2f is considered to be linear.', beam.id,
-                  cluster.m,
-                  cluster.c, len(channel_indices), cluster.score)
-
-        return cluster
-
-
-@timeit
-def _detect_clusters(beam):
+def partition_input_data(input_data, channel_noise, beam_id):
     """
-    Use the DBScan algorithm to create a set of clusters from the given beam data
 
-    :param beam: The beam object from which the clusters will be generated
+    :param input_data:
+    :param channel_noise:
+    :param beam_id:
     :return:
     """
+
+    # Process part of the input_data
+    beam_data = input_data[beam_id, :, :]
+
+    # Get the channel noise for this beam
+    beam_noise = channel_noise[beam_id]
+
+    # Calculate the SNR
+    snr = beam_data / beam_noise[:, np.newaxis]
+    snr[snr <= 0] = np.nan
+    snr = 10 * np.log10(snr)
+    snr[np.isnan(snr)] = 0.
 
     # Select the data points that are non-zero
-    ndx = np.where(beam.snr > 0)
+    ndx = np.where(snr > 0)
 
-    # Transform them in a time (x), channel (y) nd-array
-    data = np.column_stack(ndx)
+    # Transform them in a time (x), channel (y) nd-array and snr
+    return np.column_stack(ndx), snr[ndx]
 
-    # Perform DBScan on the cluster data
-    c_labels = _db_scan(beam.id, data)
 
-    log.debug('Beam %s: DBSCAN identified %s cluster labels', beam.id, len(c_labels))
+def dbscan_clustering(beam_ndx, snr_data):
+    """
+
+    :param beam_ndx:
+    :param snr_data:
+    :return:
+    """
+    try:
+        # Perform (2D) clustering on the data and returns cluster labels the points are associated with
+        c_labels = db_scan.fit_predict(beam_ndx)
+    except ValueError:
+        raise NoDetectionClustersFound
 
     if len(c_labels) < 1:
-        return []
+        raise NoDetectionClustersFound
 
-    # Select only those labels which were not classified as noise (-1)
-    filtered_c_labels = c_labels[c_labels > -1]
+    # Add cluster labels to the data
+    labelled_data = np.append(beam_ndx, np.expand_dims(c_labels, axis=1), axis=1)
 
-    log.debug('Beam %s: %s / %s data points was considered to be noise',
-              beam.id,
-              len(c_labels) - len(filtered_c_labels),
-              len(c_labels))
+    # Cluster mask to remove noise clusters
+    denoise_mask = labelled_data[:, 2] > -1
 
-    log.debug('Beam %s: %s unique clusters were detected', beam.id, len(np.unique(filtered_c_labels)))
+    # Select only those labels which were not classified as noise was(-1)
+    return labelled_data[denoise_mask], snr_data[denoise_mask]
 
-    # Group the data points in clusters
+
+def create_clusters(snr_data, labelled_data, channels, t0, td, beam_id, iter_count):
+    """
+    Group the data points in clusters
+    :param snr_data:
+    :param labelled_data:
+    :param channels:
+    :param t0:
+    :param td:
+    :param beam_id:
+    :param iter_count:
+    :return:
+    """
+
     clusters = []
-    append = clusters.append
-    unique_c_labels = np.unique(filtered_c_labels).tolist()
-    for label in unique_c_labels:
-        cluster = _create_detection_clusters(data, beam, c_labels, label)
-        if cluster:
-            append(cluster)
 
-    n_clusters = len(clusters)
-    log.info('Beam %s: Detected %s (from %s) valid clusters', beam.id, n_clusters, len(unique_c_labels))
+    unique_cluster_labels = np.unique(labelled_data[:, 2]).tolist()
 
-    # if n_clusters > 0:
-    #     _publisher.publish(SpaceDebrisClusterDetectedEvent(n_clusters, beam.id))
+    for label in unique_cluster_labels:
+        label_mask = labelled_data[:, 2] == label
+        valid, cluster = _create(snr_data[label_mask], labelled_data[label_mask], channels, t0, td, beam_id,
+                                 iter_count)
+
+        if valid:
+            clusters.append(cluster)
+            log.debug('Created beam candidate {}'.format(id(cluster)))
 
     return clusters
 
-@timeit
-def detect(obs_info, queue, beam):
+
+def detect(input_data, channels, t0, td, iter_count, channel_noise, beam_id):
     """
-    The core detection algorithm to be applied to the incoming data
+    Use the DBScan algorithm to create a set of clusters from the given beam data
 
     To be run in parallel using the multi-processing queue
 
-    :param obs_info:
-    :param queue:
-    :param beam:
-    :return: DetectionClusters
+    :param input_data:
+    :param beam_id:
+    :param channels:
+    :param t0:
+    :param td:
+    :param iter_count:
+    :param channel_noise:
+    :return:
     """
 
-    global _time_delta, _ref_time, _publisher
-    _ref_time = np.datetime64(obs_info['timestamp'])
-    _time_delta = np.timedelta64(int(obs_info['sampling_time'] * 1e9), 'ns')
+    # Process a slice of the input data
+    beam_ndx, snr_data = partition_input_data(input_data, channel_noise, beam_id)
 
-    if not _publisher:
-        _publisher = EventsPublisher.Instance()
+    try:
+        # Run the clustering algorithm on the beam data (channel,time,snr,label)
+        labelled_data, snr_data = dbscan_clustering(beam_ndx, snr_data)
+    except NoDetectionClustersFound:
+        log.debug('No detection clusters were found in iteration {}'.format(iter_count))
+        return []
 
-    # Associate a beam candidate repository to the queue
-    # queue.set_repository(BeamCandidateRepository())
+    # Validate and create linear detection clusters
+    clusters = create_clusters(snr_data, labelled_data, channels, t0, td, beam_id, iter_count)
+    log.debug('Beam %s: %s: %s valid clusters remain after validation', beam_id, iter_count, len(clusters))
 
-    # Save the raw data as a fits file
-    # save_fits(beam.snr, 'raw', beam.id)
-
-    # Apply the pre-processing filters to the beam data
-    # beam.apply_filters()
-
-    # Save the filtered data as a fits file
-    # save_fits(beam.snr, 'filtered', beam.id)
-
-    return _detect_clusters(beam)
-
-    # Add the beam candidates to the beam queue
-    enqueue = queue.enqueue
-    for candidate in _detect_clusters(beam):
-        enqueue(candidate)
-
-    # Get the detection candidates
-    return queue.get_candidates(beam.id)
+    return clusters

pybirales/pipeline/modules/detection/detector.py
@@ -1,20 +1,17 @@
 import logging as log
-import Queue
 from functools import partial
 from multiprocessing import Pool
 
 import numpy as np
+from scipy import stats
 
 from pybirales import settings
-from pybirales.events.events import SpaceDebrisDetectedEvent
-from pybirales.events.publisher import EventsPublisher
 from pybirales.pipeline.base.processing_module import ProcessingModule
 from pybirales.pipeline.blobs.channelised_data import ChannelisedBlob
-from pybirales.pipeline.modules.detection.beam import Beam
 from pybirales.pipeline.modules.detection.dbscan_detection import detect
-from pybirales.pipeline.modules.detection.queue import BeamCandidatesQueue
+from pybirales.pipeline.modules.detection.exceptions import DetectionClusterIsNotValid
 from pybirales.pipeline.modules.detection.space_debris_candidate import SpaceDebrisTrack
-from pybirales.pipeline.modules.detection.tdm.tdm import TDMWriter
+from numba import jit
 
 
 class Detector(ProcessingModule):
@@ -24,23 +21,13 @@ class Detector(ProcessingModule):
         # Ensure that the input blob is of the expected format
         self._validate_data_blob(input_blob, valid_blobs=[ChannelisedBlob])
 
-        # Data structure that hold the detected debris (for merging)
-        self._debris_queue = BeamCandidatesQueue(settings.beamformer.nbeams)
-
-        # Flag that indicates whether the configuration was persisted
-        self._config_persisted = False
-
         self.pool = None
         if settings.detection.multi_proc:
-            self.pool = Pool(settings.detection.n_procs)
+            self.pool = Pool(4)
 
         self.counter = 0
 
         self.channels = None
-
-        self.time = None
-
-        self.channels_i = None
 
         self._doppler_mask = None
 
@@ -48,16 +35,8 @@ class Detector(ProcessingModule):
 
         self.name = "Detector"
 
-        self._candidates_detected = 0
-
-        self._publisher = EventsPublisher.Instance()
-
         # A list of space debris tracks
         self._candidates = []
-
-        self._tdm_writer = TDMWriter(queue=Queue.Queue())
-
-        self._tdm_writer.start()
 
         self._detection_counter = 0
 
@@ -69,120 +48,93 @@ class Detector(ProcessingModule):
         self._write_freq = 10
 
     def _tear_down(self):
-        self._tdm_writer.stop()
-        self.pool.close()
-
-    def _get_doppler_mask(self, tx, channels):
-        if self._doppler_mask is None:
-            a = tx + settings.detection.doppler_range[0] * 1e-6
-            b = tx + settings.detection.doppler_range[1] * 1e-6
-
-            self._doppler_mask = np.bitwise_and(channels < b, channels > a)
-
-        return self._doppler_mask
-
-    def _get_channels(self, obs_info):
-        if self.channels is None:
-            self.channels = np.arange(obs_info['start_center_frequency'],
-                                      obs_info['start_center_frequency'] + obs_info['channel_bandwidth'] * obs_info[
-                                          'nchans'],
-                                      obs_info['channel_bandwidth'])
-            self.channels_i = np.arange(0, obs_info['nchans'])
-            if settings.detection.doppler_subset:
-                doppler_mask = self._get_doppler_mask(obs_info['transmitter_frequency'], self.channels)
-                self.channels = self.channels[doppler_mask]
-                self.channels_i = self.channels_i[doppler_mask]
-
-        return self.channels, self.channels_i
-
-    def _get_time(self, obs_info):
-        self.time = np.arange(0, obs_info['nsamp']) + self.counter * obs_info['nsamp']
-
-        return self.time
-
-    def process(self, obs_info, input_data, output_data):
         """
-        Run the Space Debris Detector pipeline
+
+        :return:
+        """
+        if settings.detection.multi_proc:
+            self.pool.close()
+
+    def _apply_doppler_mask(self, obs_info):
+        """
 
         :param obs_info:
-        :param input_data:
-        :param output_data:
+        :return:
+        """
+        if self._doppler_mask is None:
+            self.channels = np.arange(obs_info['start_center_frequency'],
+                                      obs_info['start_center_frequency'] + obs_info['channel_bandwidth'] * obs_info[
+                                          'nchans'], obs_info['channel_bandwidth'])
+            a = obs_info['transmitter_frequency'] + settings.detection.doppler_range[0] * 1e-6
+            b = obs_info['transmitter_frequency'] + settings.detection.doppler_range[1] * 1e-6
+
+            self._doppler_mask = np.bitwise_and(self.channels < b, self.channels > a)
+
+            if settings.detection.enable_doppler_window:
+                self.channels = self.channels[self._doppler_mask]
+            else:
+                # Select all channels
+                self._doppler_mask = False
+
+        return self.channels, self._doppler_mask
+
+    def _debug_msg(self, cluster):
+        """
+
+        :param cluster:
+        :return:
+        """
+        m, c, r_value, _, _ = stats.linregress(cluster['channel_sample'], cluster['time_sample'])
+        return '{} (m={:0.2f}, c={:0.2f}, s={:0.2f}, n={}, i={})'.format(id(cluster), m, c, r_value, cluster.shape[0],
+                                                                         self.counter)
+
+    def _aggregate_clusters(self, candidates, clusters, obs_info):
+        """
+        Create Space Debris Tracks from the detection clusters identified in each beam
+
+        :param clusters:
+        :return:
+        """
+        for cluster_list in clusters:
+            for cluster in cluster_list:
+                for candidate in candidates:
+                    # If beam candidate is similar to candidate, merge it.
+                    if candidate.is_parent_of(cluster):
+                        try:
+                            candidate.add(cluster)
+                        except DetectionClusterIsNotValid:
+                            log.debug('Beam candidate {} could not be added to track {}'.format(
+                                self._debug_msg(cluster), id(candidate)))
+                        else:
+                            log.debug('Beam candidate {} added to track {}'.format(self._debug_msg(cluster),
+                                                                                   id(candidate)))
+                            break
+                else:
+                    # Beam candidate does not match any candidate. Create candidate from it.
+                    # Transform this beam candidate into a space debris track
+                    try:
+                        sd = SpaceDebrisTrack(obs_info=obs_info, cluster=cluster)
+                    except DetectionClusterIsNotValid:
+                        continue
+
+                    log.debug('Created new track {} from Beam candidate {}'.format(id(sd), self._debug_msg(cluster)))
+
+                    # Add the space debris track to the candidates list
+                    candidates.append(sd)
+
+        return candidates
+
+    def _active_tracks(self, candidates, iter_count):
+        """
+
+        :param candidates:
+        :param iter_count:
         :return:
         """
 
-        # Skip the first two blobs
-        if self.counter < 2:
-            self.counter += 1
-            return
-
-        obs_info['iter_count'] = self.counter
-        channels, channels_i = self._get_channels(obs_info)
-        time = self._get_time(obs_info)
-        doppler_mask = self._get_doppler_mask(obs_info['transmitter_frequency'], channels)
-
-        if settings.detection.doppler_subset:
-            input_data = input_data[:, doppler_mask, :]
-
-        beams = [Beam(beam_id=n_beam,
-                      obs_info=obs_info,
-                      channels=channels,
-                      channels_i=channels_i,
-                      time=time,
-                      beam_data=input_data)
-                 for n_beam in range(settings.detection.beam_range[0], settings.detection.beam_range[1])]
-
-        if settings.detection.multi_proc:
-            func = partial(detect, obs_info, self._debris_queue)
-            beam_candidates = self.pool.map(func, beams)
-        else:
-            beam_candidates = []
-            for beam in beams:
-                beam_candidates.append(detect(obs_info, self._debris_queue, beam))
-
-        self._candidates_detected += np.count_nonzero(beam_candidates)
-
-        log.info('Result: Detected {} beam candidates across {} beams'.format(self._candidates_detected, len(beams)))
-
-        self._debris_queue.set_candidates(beam_candidates)
-
-        # Create Space Debris Tracks from tracks in beams
-        for beam_candidate_list in beam_candidates:
-            for beam_candidate in beam_candidate_list:
-                for candidate in self._candidates:
-                    # If beam candidate is similar to candidate, merge it.
-                    if candidate.is_parent_of(beam_candidate) and beam_candidate.is_linear and beam_candidate.is_valid:
-                        log.debug(
-                            'Beam candidate {} (m={}, c={}, s={}, n={}) added to track {}'.format(id(beam_candidate),
-                                                                                                  beam_candidate.m,
-                                                                                                  beam_candidate.c,
-                                                                                                  beam_candidate.score,
-                                                                                                  len(
-                                                                                                      beam_candidate.time_data),
-                                                                                                  id(candidate)))
-
-                        candidate.add(beam_candidate)
-                        break
-                else:
-                    # Beam candidate does not match any candidate. Create candidate from it.
-                    if beam_candidate.is_linear and beam_candidate.is_valid:
-                        # Transform this beam candidate into a space debris track
-                        self._detection_counter += 1
-                        sd = SpaceDebrisTrack(det_no=self._detection_counter, obs_info=obs_info,
-                                              beam_candidate=beam_candidate)
-
-                        log.debug('Created new track {} from Beam candidate {}'.format(id(sd), id(beam_candidate),
-                                                                                       beam_candidate.debug_msg))
-                        # Publish event: A space debris detection was made
-                        self._publisher.publish(SpaceDebrisDetectedEvent(sd))
-
-                        # Add the space debris track to the candidates list
-                        self._candidates.append(sd)
-                    else:
-                        log.debug('Beam candidate {} is not linear or not valid'.format(id(beam_candidate)))
-
         temp_candidates = []
-        for c in self._candidates:
-            if c.is_finished(current_time=self.last_time_sample, min_channel=channels[0], iter_count=self.counter):
+        for c in candidates:
+            if c.is_finished(current_time=10, min_channel=20, iter_count=iter_count):
                 log.debug('Track {} (n: {}) has transitted outside detection window. Removing it from candidates list'
                           .format(id(c), c.size))
 
@@ -196,19 +148,88 @@ class Detector(ProcessingModule):
 
         log.info('Result: {} tracks have transitted. {} currently in detection window.'.format(
             len(self._candidates) - len(temp_candidates), len(temp_candidates)))
-        self._candidates = temp_candidates
+
+        return temp_candidates
+
+    def _pre_process(self, input_data, obs_info):
+        """
+
+        :param input_data:
+        :param obs_info:
+        :return:
+        """
+        channels, doppler_mask = self._apply_doppler_mask(obs_info)
+        channel_noise = obs_info['channel_noise']
+
+        if settings.detection.enable_doppler_window:
+            input_data = input_data[:, doppler_mask, :]
+            channel_noise = channel_noise[:, doppler_mask]
+
+        log.info('Using {} out of {} channels ({:0.3f} MHz to {:0.3f} MHz).'.format(channel_noise.shape[1],
+                                                                                    obs_info['nchans'], channels[0],
+                                                                                    channels[-1]))
+
+        return input_data, channels, channel_noise
+
+    def _get_clusters(self, input_data, channels, channel_noise, obs_info, iter_counter):
+        """
+
+        :param input_data:
+        :param channels:
+        :param channel_noise:
+        :param obs_info:
+        :param iter_counter:
+        :return:
+        """
+
+        t0 = np.datetime64(obs_info['timestamp'])
+        td = np.timedelta64(int(obs_info['sampling_time'] * 1e9), 'ns')
+
+        clusters = []
+        for beam_id in range(0, 32):
+            clusters.append(detect(input_data, channels, t0, td, iter_counter, channel_noise, beam_id))
+
+        # todo - multi-processing is slower than single threaded equivalent
+        # if settings.detection.multi_proc:
+        #     func = partial(detect, input_data, channels, t0, td, iter_counter, channel_noise)
+        #     clusters = self.pool.map(func, range(0, 32))
+        #     return clusters
+
+
+        return clusters
+
+    def process(self, obs_info, input_data, output_data):
+        """
+        Sieve through the pre-processed channelised data for space debris candidates
+
+        :param obs_info:
+        :param input_data:
+        :param output_data:
+        :return:
+        """
+
+        # Skip the first two blobs
+        if self.counter < 2:
+            self.counter += 1
+            return
+
+        obs_info['iter_count'] = self.counter
+
+        # Pre-process the input data
+        input_data, channels, channel_noise = self._pre_process(input_data, obs_info)
+
+        # Process the input data and identify the detection clusters
+        clusters = self._get_clusters(input_data, channels, channel_noise, obs_info, self.counter)
+
+        # Create new tracks from clusters or merge clusters into existing tracks
+        candidates = self._aggregate_clusters(self._candidates, clusters, obs_info)
+
+        # Check each track and determine if the detection object has transitted outside FoV
+        self._candidates = self._active_tracks(candidates, self.counter)
 
         self.counter += 1
 
-        if self.counter % self._write_freq == 0:
-            for c in self._candidates:
-                self._tdm_writer.queue.put((c, obs_info))
-
         return obs_info
-
-    @property
-    def last_time_sample(self):
-        return self.time[0]
 
     def generate_output_blob(self):
         pass

pybirales/pipeline/modules/detection/filter.py
@@ -79,12 +79,7 @@ class RemoveTransmitterChannelFilter(InputDataFilter):
             summed = np.sum(data, axis=2)
             peaks_snr_i = np.unique(np.where(summed > np.mean(summed) + np.std(summed) * 2.0)[1])
             data[:, peaks_snr_i, :] = 0.0
-
-        # print(peaks_snr_i)
-        # print(data.shape)
-        # print(np.sqrt(np.mean(data ** 2)))
-
-        log.debug('Transmitter frequency removed')
+            log.debug('Transmitter frequency filter applied')
 
 
 class Filter(ProcessingModule):

pybirales/pipeline/modules/detection/preprocessor.py
@@ -1,3 +1,5 @@
+import logging as log
+
 import numpy as np
 
 from pybirales import settings
@@ -15,7 +17,7 @@ class PreProcessor(ProcessingModule):
 
         self.counter = 0
 
-        self.noise = []
+        self.channel_noise = np.empty(shape=(32, 8192, settings.detection.n_noise_samples)) * np.nan
 
         self._observation = None
 
@@ -26,20 +28,17 @@ class PreProcessor(ProcessingModule):
 
         self.name = "PreProcessor"
 
-    def _get_noise_estimation(self, data):
+    def _get_noise_estimation(self, power_data, iter_count):
         if self.counter < settings.detection.n_noise_samples:
-            power = self._power(data[:, settings.detection.noise_channels, :])
-
             if settings.detection.noise_use_rms:
                 #  use RMS
-                noise = np.sqrt(np.mean(power))
+                beam_channel_noise = np.sqrt(np.mean(power_data, axis=2))
             else:
                 # use mean
-                noise = np.mean(power)
+                beam_channel_noise = np.mean(power_data, axis=2)
 
-            self.noise.append(noise)
-            self.mean_noise = np.mean(self.noise)
-        return float(self.mean_noise)
+            self.channel_noise[:, :, iter_count] = beam_channel_noise
+        return np.nanmean(self.channel_noise, axis=2)
 
     def process(self, obs_info, input_data, output_data):
         """
@@ -51,20 +50,25 @@ class PreProcessor(ProcessingModule):
         :return:
         """
 
-        # Reduce the blob to a single polarization
+        # Process only 1 polarisation
         data = input_data[0, :, :, :]
 
+        power_data = self._power(data)
+
         # Estimate the noise from the data
-        obs_info['noise'] = self._get_noise_estimation(data)
+        obs_info['channel_noise'] = self._get_noise_estimation(power_data, self.counter)
+        obs_info['mean_noise'] = np.mean(obs_info['channel_noise'])
 
         # If the configuration was not saved AND the number of noise samples is sufficient, save the noise value.
         if not self._config_persisted and self.counter >= settings.detection.n_noise_samples:
             self._observation = Observation.objects.get(id=settings.observation.id)
-            self._observation.noise_estimate = obs_info['noise']
+            self._observation.mean_noise = obs_info['mean_noise']
+            self._observation.mean_channel_noise = obs_info['channel_noise']
             self._observation.save()
             self._config_persisted = True
 
-        output_data[:] = self._power(data)
+            log.info('Mean noise {:0.3f}'.format(obs_info['mean_noise']))
+        output_data[:] = power_data
         self.counter += 1
 
         return obs_info

pybirales/pipeline/modules/detection/space_debris_candidate.py
@@ -2,22 +2,14 @@ import datetime
 import logging as log
 
 import numpy as np
-
-import warnings
-
-warnings.simplefilter('ignore', category=FutureWarning)
-
 import pandas as pd
-import warnings
-
-warnings.simplefilter('ignore', category=FutureWarning)
-
 import scipy.spatial.distance as dist
 from mongoengine import *
+from scipy import stats
 from sklearn import linear_model
 
 from pybirales import settings
-from pybirales.pipeline.modules.detection.detection_clusters import DetectionCluster
+from pybirales.pipeline.modules.detection.exceptions import DetectionClusterIsNotValid
 from pybirales.repository.models import SpaceDebrisTrack
 
 _linear_model = linear_model.RANSACRegressor(linear_model.LinearRegression())
@@ -46,14 +38,13 @@ class Target:
 
 
 class SpaceDebrisTrack:
-    def __init__(self, det_no, obs_info, beam_candidate=None):
+    def __init__(self, obs_info, cluster=None):
         """
 
         :param obs_info:
-        :param beam_candidate:
+        :param cluster:
         """
         self._id = None
-        self.detection_num = det_no
         self.name = None
         self._created_at = datetime.datetime.utcnow()
         self._obs_info = obs_info
@@ -78,7 +69,7 @@ class SpaceDebrisTrack:
         self._detected_target = None
 
         # DataFrame encapsulating the track data (snr, time, channel, beam id)
-        self.data = pd.DataFrame(columns=['time_sample', 'channel_sample',  'time', 'channel', 'snr', 'beam_id'])
+        self.data = pd.DataFrame(columns=['time_sample', 'channel_sample', 'time', 'channel', 'snr', 'beam_id'])
         # self.data.set_index('time_sample')
 
         # The filepath where to save the space debris candidate
@@ -99,8 +90,9 @@ class SpaceDebrisTrack:
         self._iter = 0
 
         # If a beam candidate is given, add it on initialisation
-        if isinstance(beam_candidate, DetectionCluster):
-            self.add(beam_candidate)
+        # if isinstance(beam_candidate, DetectionCluster):
+        #     self.add(beam_candidate)
+        self.add(cluster)
 
     @property
     def duration(self):
@@ -114,122 +106,66 @@ class SpaceDebrisTrack:
     def activated_beams(self):
         return self.data['beam_id'].unique().size
 
-    def add(self, beam_candidate):
+    def _update(self, new_df):
+        self.data = new_df
+        self.m, self.intercept, self.r_value, self.p_value, self.std_err = stats.linregress(
+            self.data['channel_sample'], self.data['time_sample'])
+
+        self._last_iter_count = self.data['iter'].max()
+
+        self.data = self.data.sort_values(by='time')
+
+        # Save the candidate
+        self._save()
+
+    def _fit(self, x, y):
+        """
+        Use a RANSAC linear fitting model to remove outliers
+        todo - only apply RANSAC for clusters greater than a minimum number of data-points
+
+        :param x:
+        :param y:
+        :return:
+        """
+
+        try:
+            self._linear_model.fit(x.values.reshape(-1, 1), y)
+        except ValueError:
+            return False, self._linear_model
+        else:
+            return True, self._linear_model
+
+    def add(self, cluster_df):
         """
         Associate a beam candidate with this space debris track.
         Update the space debris track's attributes based on new data
 
-        :param beam_candidate:
-        :type beam_candidate: DetectionCluster
+        :param cluster_df:
+        :type cluster_df: ndarray
         :return:
         """
 
-        if not self.data.empty and not self.is_linear(beam_candidate):
-            log.warning("Beam candidate is not linear. Won't add beam candidate {}".format(id(beam_candidate)))
-            return False
+        def _merge_tmp_df(df, delta_df):
+            """
+            Create a temporary data frame (merge track df with cluster df)
+            :param df: The data frame of the track
+            :param delta_df: The cluster df that is to be merged
+            :return:
+            """
 
-        temp_df = pd.DataFrame({
-            'time_sample': beam_candidate.time_data,
-            'channel_sample': beam_candidate.channels_i,
-            'time': beam_candidate.time_f_data,
-            'channel': beam_candidate.channel_data,
-            'snr': beam_candidate.snr_data,
-            'beam_id': [beam_candidate.beam_id for _ in range(0, len(beam_candidate.time_data))],
-        })
+            if df.empty:
+                return delta_df
+            return pd.concat([self.data, cluster_df])
 
-        # Record the noise of the beam
-        self._beam_noise[beam_candidate.beam_id] = beam_candidate.beam_config['beam_noise']
+        tmp_merged_df = _merge_tmp_df(self.data, cluster_df)
+        is_valid, l_model = self._fit(tmp_merged_df['channel_sample'], tmp_merged_df['time_sample'])
 
-        if not self.data.empty:
-            # Combine the beam candidate track to this track
-            self.data = pd.concat([self.data, temp_df])
+        if is_valid:
+            # Remove outliers from the merged df and update the track
+            self._update(tmp_merged_df[l_model.inlier_mask_])
         else:
-            self.data = temp_df
-            self.score = beam_candidate.score
-            self.m = beam_candidate.m
-            self.intercept = beam_candidate.c
-
-            self._save()
-
-            self._last_iter_count = beam_candidate.iter_count
-
-            # do not run RANSAC again
-            return
-
-        # Update linear model of the track
-        channels, time = self.data['channel'].values.reshape(-1, 1), self.data['time_sample']
-
-        try:
-            self._linear_model.fit(channels, time)
-        except ValueError:
-            log.debug('Space debris linear fitting failed. Beam candidate not added to track {}'.format(id(self)))
-        else:
-            channels, time = channels[self._linear_model.inlier_mask_], time[self._linear_model.inlier_mask_]
-
-            # todo - replace the functionality below with an _update function
-            self.score = self._linear_model.estimator_.score(channels, time)
-            self.m = self._linear_model.estimator_.coef_[0]
-            self.intercept = self._linear_model.estimator_.intercept_
-
-            # Remove outliers from the data
-            self.data = self.data[self._linear_model.inlier_mask_]
-
-            # Sort data by time
-            self.data = self.data.sort_values(by='time')
-
-            # Update rcs
-            # Update detected target
-
-            # Persist the candidate to DB/TDM
-            self._save()
-
-            self._last_iter_count = beam_candidate.iter_count
-
-    def is_linear(self, detection_cluster):
-        """
-        Check that if the data from the detection cluster is added to the space debris track,
-        the linear fit works
-
-        todo - on success, you should avoid doing this twice (when adding)
-
-        :param detection_cluster:
-        :return:
-        """
-
-        temp_df = pd.DataFrame({
-            'time_sample': detection_cluster.time_data,
-            'channel_sample': detection_cluster.channels_i,
-            'channel': detection_cluster.channel_data,
-            'snr': detection_cluster.snr_data,
-
-            'beam_id': [detection_cluster.beam_id for _ in range(0, len(detection_cluster.time_data))],
-        })
-
-        temp_df2 = temp_df
-        if not self.data.empty:
-            # Combine the beam candidate track to this track
-            temp_df2 = pd.concat([self.data, temp_df])
-
-        channels, time = temp_df2['channel'].values.reshape(-1, 1), temp_df2['time_sample']
-
-        try:
-            self._linear_model.fit(channels, time)
-        except ValueError:
-            log.warning("Linear Fit failed when combining beam candidate {} to the space debris track {}".format(
-                id(detection_cluster), id(self)
-            ))
-
-            return False
-        else:
-            channels, time = channels[self._linear_model.inlier_mask_], time[self._linear_model.inlier_mask_]
-
-            score = self._linear_model.estimator_.score(channels, time)
-
-            is_linear = score > settings.detection.linearity_thold
-            doppler_check = settings.detection.m_limit[0] <= self._linear_model.estimator_.coef_[0] <= \
-                            settings.detection.m_limit[1]
-
-            return is_linear and doppler_check
+            # If fitting failed, track data remains unchanged, and raise an exception
+            raise DetectionClusterIsNotValid(cluster_df)
 
     def is_finished(self, current_time, min_channel, iter_count):
         """
@@ -242,7 +178,7 @@ class SpaceDebrisTrack:
 
         # return current_time > self._exit_time or (iter_count - self._last_iter_count) > 5
 
-        # The track is deemed to be finished if it has not been updated since N itera
+        # The track is deemed to be finished if it has not been updated since N iterations
         return (iter_count - self._last_iter_count) > self._max_candidate_iter
 
     def is_parent_of(self, detection_cluster):
@@ -252,17 +188,13 @@ class SpaceDebrisTrack:
         cosine distance.
 
         :param detection_cluster: The detection cluster with which the track is being compared
-        :type detection_cluster: DetectionCluster
         :return:
         """
 
-        # The similarity threshold which determines if track is similar or not
-        similarity_thold = settings.detection.similarity_thold
+        cluster_m, cluster_c, _, _, _ = stats.linregress(detection_cluster['channel_sample'],
+                                                         detection_cluster['time_sample'])
 
-        if self.m and self.intercept:
-            return dist.cosine([self.m, self.intercept], [detection_cluster.m, detection_cluster.c]) < similarity_thold
-
-        return True
+        return dist.cosine([self.m, self.intercept], [cluster_m, cluster_c]) < settings.detection.similarity_thold
 
     def _save(self):
         """