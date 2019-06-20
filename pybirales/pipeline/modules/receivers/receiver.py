4cf2a36b8ede7298cdbcfb212bc7bbb4dc557ffd
denis <denis0@gmail.com>
2019-06-20 11:19:28 +0200

    standalone receiver script

--- pybirales/pipeline/modules/receivers/receiver.py
+++ pybirales/pipeline/modules/receivers/receiver.py
@@ -339,18 +339,18 @@ if __name__ == "__main__":
         logging.info('Starting test %d', i)
 
         m1 = memory()
-        logging.info('Starting receiver %d. Memory Total: %f MB Diff: %f MB', i, m1, m1 - m1)
+        logging.info('Starting receiver %d. Memory Total: %.2f MB', i, m1)
         r.start_generator()
         time.sleep(2)
 
         m2 = memory()
-        logging.info('Receiver %d started. Memory Total: %f MB Diff: %f MB', i, m1, m2 - m1)
+        logging.info('Receiver %d started. Memory Total: %.2f MB. Diff: %.2f MB', i, m1, m2 - m1)
 
         r.stop()
         time.sleep(3)
 
         m3 = memory()
-        logging.info('Receiver %d stopped. Memory Total: %f MB Diff: %f MB', i, m1, m3 - m2)
+        logging.info('Receiver %d stopped. Memory Total: %.2f MB. Diff: %.2f MB', i, m1, m3 - m1)
 
 
     # Load the BIRALES configuration from file
