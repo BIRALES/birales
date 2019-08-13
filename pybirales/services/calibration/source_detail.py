03ab6a66a7be647010e681d6f82c853042d9bbb2
Denis <denis0@gmail.com>
2019-08-13 17:34:13 +0200

    calibration changes by Josef

--- pybirales/services/calibration/source_detail.py
+++ pybirales/services/calibration/source_detail.py
@@ -1,4 +1,5 @@
 import numpy as np
+import ephem
 import datetime
 from pybirales.pipeline.base.definitions import CalibrationFailedException
 import logging as log
@@ -8,36 +9,44 @@ class SourceDetail:
 
     def __init__(self, dec, time, lat, lon):
         self.source_declination = dec
+        self.source_declination2 = None
         self.source_right_ascension = None
         self.source_name = None
-        self.observation_start_time = time
-        self.observing_latitude = lat
-        self.observing_longitude = lon
+        self.observation_start_time = time 
+        log.info("Observation start time is " + str(self.observation_start_time))
+        self.observing_latitude = '44:31:24.88'
+        self.observing_longitude = '11:38:45.56'
         self.source_transit_time = None
 
         self.source_acknowledge()
-        self.source_timing()
+        self.source_timing2()
 
     def source_acknowledge(self):
+
+        log.info("Source declination {:0.3f} deg".format(self.source_declination))
         # Cassiopeia A, right ascension 23h23m24s
         if np.abs(self.source_declination - 59.) < 1.:
             log.info("Selected Cassiopeia A, right ascension 23h23m24s")
-            self.source_right_ascension = datetime.timedelta(hours=(23. + (23. / 60) + (24. / 3600)))
+            self.source_right_ascension = '23:23:24.'
+            self.source_declination2 = '58:55:27.7'
 
         # Taurus A, right ascension 05h34m32s
         elif np.abs(self.source_declination - 22.) < 1.:
             log.info("Selected Taurus A, right ascension 05h34m32s")
-            self.source_right_ascension = datetime.timedelta(hours=(5. + (34. / 60) + (32. / 3600)))
+            self.source_right_ascension = '5:34:32.'
+            self.source_declination2 = '22:01:32.5'
 
         # Virgo A, right ascension 12h30m49s
         elif np.abs(self.source_declination - 12.) < 1.:
             log.info("Selected Virgo A, right ascension 12h30m49s")
-            self.source_right_ascension = datetime.timedelta(hours=(12. + (30. / 60) + (49. / 3600)))
+            self.source_right_ascension = '12:30:49.'
+            self.source_declination2 = '12:16:54.0' 
 
         # Cygnus A, right ascension 19h59m28s
         elif np.abs(self.source_declination - 40.) < 1.:
             log.info("Selected Cygnus A, right ascension 19h59m28s")
-            self.source_right_ascension = datetime.timedelta(hours=(19. + (59. / 60) + (28. / 3600)))
+            self.source_right_ascension = '19:59:28.' 
+            self.source_declination2 = '40:47:23.3' 
 
         else:
             raise CalibrationFailedException(
@@ -50,15 +59,16 @@ class SourceDetail:
         leap_year_multip = self.observation_start_time.year % 4 / 4.
         leap_year_delta = datetime.timedelta(hours=(24. - hours_sidereal) * leap_year_multip)
         today = self.observation_start_time.date()
-        gst_0 = datetime.date(year=today.year, month=9, day=20)
+        gst_0 = datetime.date(year=today.year, month=9, day=21)
 
         if (today - gst_0).days >= 0:
-            gst_0 = datetime.date(year=today.year + 1, month=9, day=20)
+            gst_0 = datetime.date(year=today.year + 1, month=3, day=21)
 
         gst_today = np.abs((today - gst_0).days) * sidereal_day_delta
 
         longitudinal_delta = datetime.timedelta(hours=self.observing_longitude / 360. * hours_sidereal)
         lst_today = gst_today - (longitudinal_delta + sidereal_day_delta + leap_year_delta)
+        # print lst_today
 
         ha_source = (lst_today - self.source_right_ascension).seconds
         self.source_transit_time = datetime.datetime(year=self.observation_start_time.year,
@@ -66,3 +76,18 @@ class SourceDetail:
                                                      day=self.observation_start_time.day,
                                                      hour=0, minute=0, second=0) + datetime.timedelta(seconds=ha_source)
         # self.source_transit_time = datetime.datetime(year=2019, month=2, day=15, hour=13, minute=56, second=41)
+        
+    def source_timing2(self):
+      
+      telescope_location = ephem.Observer()
+      telescope_location.lat, telescope_location.lon = self.observing_latitude, self.observing_longitude 
+      telescope_location.date = self.observation_start_time
+      
+      source = ephem.FixedBody()
+      source._ra = self.source_right_ascension
+      source._dec = self.source_declination2
+      source.compute(telescope_location)
+      
+      self.source_transit_time = source.transit_time.datetime()
+
+
