15a374f12e6839aa71de4fe2f10ad74c889fcb4c
Alessio Magro <lessju@gmail.com>
2022-01-18 12:05:04 +0100

    bug fix - UTC time

--- pybirales/app/static/js/plotters/track_doppler_profile.js
+++ pybirales/app/static/js/plotters/track_doppler_profile.js
@@ -80,7 +80,7 @@ TrackDopplerProfilePlotter.prototype = {
             let tx = track['tx'];
             $.each(track['data']['channel'], function (i) {
                 track_data.push({
-                    x: moment.utc(track['data']['time'][i]),
+                    x: moment.utc(track['data']['time'][i]).format('YYYY-MM-DDTHH:mm:ss'),
                     y: (track['data']['channel'][i] - tx) * 1e6
                 })
             });
